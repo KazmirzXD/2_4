a5bf032c379b83fc6512fe7ba172ba4138833d3b
KazmirzXD <161707953+KazmirzXD@users.noreply.github.com>
2024-03-20 14:19:29 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int A;
+        int B;
+        char ZNAK; 
+
+        System.out.print("Podaj wartosc A: ");
+        A = scanner.nextInt();
+        System.out.print("Podaj wartosc B: ");
+        B = scanner.nextInt();
+
+        System.out.print("Wybierz operację (+, -, *, /) : ");
+        ZNAK = scanner.next().charAt(0);
+
+        int wynik = 0;
+        switch (ZNAK) {
+            case '+':
+                wynik = A + B;
+                break;
+            case '-':
+                wynik = A - B;
+                break;
+            case '*':
+                wynik = A * B;
+                break;
+            case '/':
+                wynik = A / B;
+                break;
+            default:
+                System.out.println("Niepoprawna operacja");
+                break;
+        }
+
+        System.out.printf("Wynik operacji to: %d \n", wynik);
+    }
+
+}
